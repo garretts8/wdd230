4a9e16de804dcf9ff67e168ff75307e50852e1a1
garretts8 <gar21085@byui.edu>
2022-09-21 22:10:34 -0600

    Clean up code

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -29,19 +29,12 @@ const todaysDate = new Date();
 // const dayName = dayNames[todaysDate.getDay()];
 // console.log(dayName)
 const dateName = todaysDate.getDate();
-console.log(dateName)
 const monthName = monthNames[todaysDate.getMonth()];
-console.log(monthName)
 const year = todaysDate.getFullYear();
-console.log(year)
 const timeHour = todaysDate.getHours(); 
-console.log(timeHour)
 const timeMinute = todaysDate.getMinutes();
-console.log(timeMinute)
 const timeSecond = todaysDate.getSeconds();
-console.log(timeSecond)
 const currentDate = `${monthName}/${dateName}/${year} ${timeHour.toString().padStart(2, '0')}:${timeMinute.toString().padStart(2, '0')}:${timeSecond.toString().padStart(2, '0')} `;
 // //const currentDate = `${monthName}/${dayName}/${todaysDate.getFullYear()} ${time} `; // Used to print AM/PM formated time
-console.log(currentDate)
 
 document.getElementById('currentdate').textContent = currentDate;
