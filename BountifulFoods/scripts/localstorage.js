07684fc635e55c22d6fdb3cede28d636d79c84ef
garretts8 <gar21085@byui.edu>
2022-12-13 21:37:22 -0700

    Completed about us page

---
+++ BountifulFoods/scripts/localstorage.js
@@ -0,0 +1,15 @@
+/////////To calculate days since last visit/////////////
+let visit = window.localStorage.getItem("latestVisit");
+let vis = document.getElementById("VisitedLast");
+let visits = 0;
+const today = Date.now();
+if (visit == null || visit == undefined) {
+	localStorage.setItem("latestVisit", today);
+	vis.textContent = visits;
+} else if (visit !== 0) {
+		const dayLastVisit = today - visit;
+		const oneDay = 24 * 60 * 60 * 1000;
+		const days = Math.round(dayLastVisit / oneDay); 
+		vis.textContent = days;
+		localStorage.setItem("latestVisit", today);
+};
